4ec473ad06da187ee004b1d3d60f7e4cb97cea93
ertgumil <ertgumil@gmail.com>
2015-10-07 15:29:22 +0200

    Add warning message in Python

--- test.py
+++ test.py
@@ -1,6 +1,9 @@
 import unittest
+import warnings
+
 class DemoTest(unittest.TestCase):
     def test_passes(self):
+        warnings.warn("Warning...........Message")
         pass
     def test_fails(self):
         self.fail("I failed.")
